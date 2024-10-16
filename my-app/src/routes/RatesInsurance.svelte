93f49f88a11df0d56775e64c464601dc0381b743
CandyKaine1 <thejohnmichaelkane@gmail.com>
2024-10-15 22:02:25 -0400

    Added more pages

---
+++ my-app/src/routes/RatesInsurance.svelte
@@ -0,0 +1,2 @@
+<h1>SELF-PAY OPTIONS</h1>>
+<p>We require payment at the time of the session unless other arrangements have been made in advance.  Cash, check, and all major credit cards are accepted for payment.</p>
