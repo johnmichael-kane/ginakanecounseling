b445a4989657e05fa05a821582c41e198f895fb9
CandyKaine1 <thejohnmichaelkane@gmail.com>
2024-10-15 23:40:29 -0400

    update

--- my-app/src/routes/NewClients.svelte
+++ my-app/src/routes/NewClients.svelte
@@ -1,1 +1,3 @@
 <h>New Clients</h>
+<!--for new people I'd prefer something on me about the page saying (BOOK AN APPOINTMENT) OR (NEW INFORMATION), where they fill out a block
+Name, Email, Square with them to add where it's written (AUTOMATES AN EMAIL TO ginalk61@gmail.COM) with their info-->
